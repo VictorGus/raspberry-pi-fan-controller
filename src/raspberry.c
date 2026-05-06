6d98914408e0c1518e1e930c685f5ec5ff3a4c22
VictorGus <retaow@gmail.com>
2026-05-06 12:17:33 +0300

    add: dummy handlers

--- src/raspberry.c
+++ src/raspberry.c
@@ -1,17 +1,29 @@
-#include "../include/raspberry.h"
+#include "raspberry.h"
+
+static int sim_fan_on = 0;
+static int sim_temp = 35;
 
 int get_fan_pin() {
-  return 1;
-};
+  return 4;
+}
 
 int turn_on_fan(int pin) {
-  return 1;
-};
+  (void)pin;
+  sim_fan_on = 1;
+  return FAN_ON_SUCCESS;
+}
 
 int turn_off_fan(int pin) {
-  return 1;
-};
+  (void)pin;
+  sim_fan_on = 0;
+  return FAN_OFF_SUCCESS;
+}
 
 int get_temperature() {
-  return 1;
-};
+  if (sim_fan_on) {
+    if (sim_temp > 30) sim_temp--;
+  } else {
+    if (sim_temp < 80) sim_temp++;
+  }
+  return sim_temp;
+}
